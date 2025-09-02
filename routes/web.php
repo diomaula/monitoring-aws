90893ee87e99273b9f25ba8358d395ef16c9d6e3
Dio Riza Maula <diomaula10@gmail.com>
2025-09-02 23:12:18 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,4 @@ Route::get('/api/stations', [AwsController::class, 'stations']);
 Route::get('/api/aws/weekly-average', [AwsController::class, 'getWeeklyAverage']);
 Route::get('/api/aws/weekly-multi', [AwsController::class, 'weeklyMultiParameter']);
 Route::get('/aws/{id}', [AwsController::class, 'show']);
-Route::get('/report', [ReportController::class, 'index']);
+// Route::get('/report', [ReportController::class, 'index']);
