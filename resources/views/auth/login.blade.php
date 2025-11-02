cf9a0d3ce28f2c8261fadd4967f7858fc50207f3
Dio Riza Maula <diomaula10@gmail.com>
2025-11-02 15:41:09 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -424,15 +424,17 @@
             }
         }
     </style>
+
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css" />
     <link href="{{ asset('assets/img/icon.png') }}" rel="icon">
-    <title>Sign in / Sign up Form</title>
+    <title>Stamet-Banyuwangi | Login</title>
     <!-- Font Awesome 5 -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
   </head>
+
   <body>
     <div class="container">
       <div class="forms-container">
